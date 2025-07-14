2a6c0ae91b96bd224af4f1cbf43862d4d2e3f512
Abdullah Fareed <your-email@example.com>
2025-07-14 09:57:07 +0500

    Resolved conflicts and added latest changes

---
+++ watches-react-app/watches-react-app/src/context/Reducer.jsx
@@ -0,0 +1,41 @@
+export const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.payload, qty: 1 }],
+      };
+
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter(
+          (c) =>
+            !(
+              c.id === action.payload.id &&
+              c.selectedSize === action.payload.selectedSize
+            )
+        ),
+      };
+
+    case "CLEAR_CART":
+      return {
+        ...state,
+        cart: [],
+      };
+
+    case "CHANGE_CART_QTY":
+      return {
+        ...state,
+        cart: state.cart.map((c) =>
+          c.id === action.payload.id
+            ? { ...c, qty: action.payload.qty }
+            : c
+        ),
+      };
+
+
+    default:
+      return state;
+  }
+};
--- watches-react-app/watches-react-app/vite.config.js
+++ watches-react-app/watches-react-app/vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    outDir: "dist", // 👈 ensures Netlify knows where to find the build
+  }
 });
